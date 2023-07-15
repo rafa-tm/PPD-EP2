0b431be235926008b83de210fe04593fe025403a
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-07-15 19:27:54 -0300

    Inicio ep3

---
+++ ep3/job.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J pi_ppd                   # Job name
+#SBATCH -p fast                     # Job partition
+#SBATCH -n 1                        # Number of processes
+#SBATCH -t 01:30:00                 # Run time (hh:mm:ss)
+#SBATCH --cpus-per-task=40          # Number of CPUs per process
+#SBATCH --output=%x.out          # Name of stdout output file - %j expands to jobId and %x to jobName
+#SBATCH --error=%x.err           # Name of stderr output file
+
+for i in {512,1024,2048};
+ do
+    echo "    "
+    echo "*** SEQUENTIAL ***"
+    srun singularity run container.sif laplace_seq $i
+    for j in {1,2,5,10,20,40};
+        do
+            echo "*** OPENMP COM $j THREADS ***"
+            export OMP_NUM_THREADS=$j
+            srun singularity run container.sif laplace_omp $i
+            echo "    "
+            echo "*** OPENMP COLLAPSE COM $j THREADS ***"
+            export OMP_NUM_THREADS=$j
+            srun singularity run container.sif laplace_omp_collapse $i
+            echo "    "
+        done
+    echo "    "
+ done
