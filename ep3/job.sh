9127868d58216dd7f4131ea1fd267b15c65463a8
Rafael Tury Minatel <rafaelturyminatel@gmail.com>
2023-08-19 13:40:03 -0300

    fix out

--- ep3/job.sh
+++ ep3/job.sh
@@ -3,6 +3,7 @@
 #SBATCH --partition=fast             # Partition (queue) name
 #SBATCH -n 1                        # Number of processes
 #SBATCH -t 01:30:00                 # Run time (hh:mm:ss)
+#SBATCH --cpus-per-task=96          # Number of CPUs per task
 #SBATCH --output=%x.out          # Name of stdout output file - %j expands to jobId and %x to jobName
 #SBATCH --error=%x.err           # Name of stderr output file
 
@@ -10,14 +11,13 @@
     echo "    "
     echo "*** SEQUENTIAL ***"
     srun singularity run container.sif laplace_seq 2048
-    for j in {1,2,5,10,20,40};
+    for j in {1,2,5,10,20,40,64};
         do
-            echo "*** OPENMP COM $j THREADS ***"
             export OMP_NUM_THREADS=$j
+            echo "*** OPENMP COM $j THREADS ***"
             srun singularity run container.sif laplace_omp 2048
             echo "    "
             echo "*** OPENMP COLLAPSE COM $j THREADS ***"
-            export OMP_NUM_THREADS=$j
             srun singularity run container.sif laplace_omp_collapse 2048
             echo "    "
         done
